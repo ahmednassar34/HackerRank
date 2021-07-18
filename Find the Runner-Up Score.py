8a4b54afb4a0e8440aa03832ea8d8e9a6e0680cc
ahmednassar34 <ahmedalinassar34@gmail.com>
2021-07-18 15:56:51 +0200

    HackerRank Problems

---
+++ Find the Runner-Up Score.py
@@ -0,0 +1,8 @@
+
+# Find the Runner-Up Score! in Python - Hacker Rank Solution
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    # Find the Runner-Up Score! in Python - Hacker Rank Solution START
+    print(sorted(list(set(arr)))[-2])
+    # Find the Runner-Up Score! in Python - Hacker Rank Solution END
